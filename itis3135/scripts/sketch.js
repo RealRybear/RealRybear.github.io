28eaa7bd416fda2c2dc5cae06a792a7de9da1420
R Crosby <ryancrosby129@gmail.com>
2025-04-28 09:44:54 -0400

    added interactive drawing html

---
+++ itis3135/scripts/sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+    createCanvas(710, 400, WEBGL);
+    angleMode(DEGREES);
+    strokeWeight(5);
+    noFill();
+    stroke(32, 8, 64);
+    describe(
+    );
+  }
+  
+  function draw() {
+    background(0);
+  
+    // Call every frame to adjust camera based on mouse/touch
+    orbitControl();
+  
+    // Rotate rings in a half circle to create a sphere of cubes
+    for (let zAngle = 0; zAngle < 180; zAngle += 30) {
+      // Rotate cubes in a full circle to create a ring of cubes
+      for (let xAngle = 0; xAngle < 360; xAngle += 30) {
+        push();
+  
+        // Rotate from center of sphere
+        rotateZ(zAngle);
+        rotateX(xAngle);
+  
+        // Then translate down 400 units
+        translate(0, 400, 0);
+        box();
+        pop();
+      }
+    }
+  }
